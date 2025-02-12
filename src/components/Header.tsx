248bfa81040e736f67de3408e306b25027a91baf
Stanleyzenitram <stanley.25@hotmail.com>
2025-02-12 01:44:07 -0400

    estilo header mejorado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,19 +9,19 @@ export default function Header() {
                     <img
                         src="/src/assets/images/icon.png"
                         alt="icon"
-                        className="h-20 mt-4 ml-4 px-5"
+                        className="h-20 ml-4 px-5"
                     />
                 </a>
 
                 <a
                     href="#"
-                    className="text-2xl ml-4 mt-4 text-amber-900 font-light"
+                    className="text-2xl ml-4  text-amber-900 font-light hover:text-amber-600 transition"
                 >
                     Tiles Import & Export S.R.L.
                 </a>
             </div>
 
-            <div className="grid grid-cols-2  items-center font-medium text-amber-900 uppercase place-content-end mb-4">
+            <div className="flex  items-center font-medium text-amber-900 uppercase place-content-end h-30">
                 <div className="flex items-center justify-end">
                     <a
                         href="#"
@@ -36,7 +36,7 @@ export default function Header() {
                         Sobre nosotros
                     </a>
                 </div>
-                <div className="flex items-center justify-end mr-4">
+                <div className="flex items-center justify-end mr-4 ">
                     <button className="w-12 h-12 flex items-center justify-center text-2xl text-amber-900 rounded-lg hover:text-amber-600 transition">
                         <FontAwesomeIcon icon={faShoppingCart} size="lg" />
                     </button>
