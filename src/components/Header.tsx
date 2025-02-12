4e74a36f5cadac0b4c481390a9a3db95a1800566
Stanleyzenitram <stanley.25@hotmail.com>
2025-02-12 03:21:14 -0400

    react routes implementado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,50 +1,79 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart, faUser } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
     return (
         <div className="h-30 bg-amber-400 grid grid-cols-2 place-content-between">
+            {/* Logo y Nombre */}
             <div className="flex items-center">
-                <a href="#">
+                <NavLink to="/">
                     <img
                         src="/src/assets/images/icon.png"
                         alt="icon"
                         className="h-20 ml-4 px-5"
                     />
-                </a>
+                </NavLink>
 
-                <a
-                    href="#"
+                <NavLink
+                    to="/"
                     className="text-2xl ml-4  text-amber-900 font-light hover:text-amber-600 transition"
                 >
                     Tiles Import & Export S.R.L.
-                </a>
+                </NavLink>
             </div>
-
-            <div className="flex  items-center font-medium text-amber-900 uppercase place-content-end h-30">
-                <div className="flex items-center justify-end">
-                    <a
-                        href="#"
-                        className="mr-6 hover:text-amber-600 transition"
+            {/* Enlaces + Iconos */}
+            <nav className="flex  items-center font-medium text-amber-900 uppercase place-content-end h-30">
+                <div className="flex items-center justify-end font-bold">
+                    <NavLink
+                        to="/"
+                        className= {({isActive}) => isActive? "text-amber-600 underline mr-6" :  "mr-6 hover:text-amber-600 transition"}
                     >
                         Inicio
-                    </a>
-                    <a
-                        href="#"
-                        className="mr-6 hover:text-amber-600 transition"
+                    </NavLink>
+                    <NavLink
+                        to="/sobreNosotros"
+                        className= {({isActive}) => isActive? "text-amber-600 underline mr-6" :  "mr-6 hover:text-amber-600 transition"}
                     >
                         Sobre nosotros
-                    </a>
+                    </NavLink>
                 </div>
-                <div className="flex items-center justify-end mr-4 ">
+                {/* Iconos */}
+                <div className="flex items-center justify-end mr-4 relative">
                     <button className="w-12 h-12 flex items-center justify-center text-2xl text-amber-900 rounded-lg hover:text-amber-600 transition">
                         <FontAwesomeIcon icon={faShoppingCart} size="lg" />
                     </button>
-                    <button className="w-12 h-12 flex items-center justify-center text-2xl text-amber-900 rounded-lg hover:text-amber-600 transition">
-                        <FontAwesomeIcon icon={faUser} size="lg" />
-                    </button>
+
+                    {/* Icono de usuario con menú */}
+                    <div className="group relative">
+                        <button className="w-12 h-12 flex items-center justify-center text-2xl text-amber-900 rounded-lg hover:text-amber-600 transition">
+                            <FontAwesomeIcon icon={faUser} size="lg" />
+                        </button>
+
+                        {/* Menú desplegable */}
+                        <div className="absolute right-0 w-52 bg-amber-400 shadow-lg rounded-lg py-2 text-gray-800 hidden group-hover:block z-10">
+                            <NavLink
+                                to="/login"
+                                className="block px-4 py-2 hover:bg-gray-200"
+                            >
+                                Iniciar sesión
+                            </NavLink>
+                            <NavLink
+                                to="#"
+                                className="block px-4 py-2 hover:bg-gray-200"
+                            >
+                                Registrarse
+                            </NavLink>
+                            <NavLink
+                                to="#"
+                                className="block px-4 py-2 hover:bg-gray-200"
+                            >
+                                Cerrar sesión
+                            </NavLink>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </nav>
         </div>
     );
 }
---
+++ src/router.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/Login";
+import Layout from "./layouts/Layout";
+
+export default function AppRouter() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<Layout />}>
+                    <Route path="/" element={<HomePage />} index/>
+                    <Route path="/login" element={<LoginPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
