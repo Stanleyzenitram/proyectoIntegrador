8112f552393b6838731a6c8ca338a7f05b472c96
Stanleyzenitram <stanley.25@hotmail.com>
2025-02-17 01:10:30 -0400

    menuMantenimientos

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -5,7 +5,7 @@ export default function Layout() {
         <>
             <Header />
             {/*pt-30 */}
-            <main className="container mx-auto  ">
+            <main className="container mx-auto pt-25 ">
                 <Outlet />
             </main>
         </>
